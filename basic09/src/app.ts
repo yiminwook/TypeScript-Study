d496e192b6861f911ed58a69cea11caa32a0b352
yiminwook <grs0416@gmail.com>
2023-02-13 02:27:28 +0900

    Add: Google Geocode API

--- basic09/src/app.ts
+++ basic09/src/app.ts
@@ -1,10 +1,37 @@
+import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-function searchAddressHandler(e: Event) {
+declare const GOOGLE_API_KEY: string | undefined;
+
+type GoolgeGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESUlt';
+};
+
+async function searchAddressHandler(e: Event) {
   e.preventDefault();
   //send to this to google API
   const enteredAddress = addressInput.value;
+
+  if (GOOGLE_API_KEY === undefined) {
+    throw Error('API_KEY is Empty!');
+  }
+  try {
+    //Google map geocoding API
+    const result = await axios.get<GoolgeGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress,
+      )}&key=${GOOGLE_API_KEY}`,
+    );
+
+    if (result.data.status !== 'OK') {
+      throw new Error('could not fetch location');
+    }
+    const coordinates = result.data.results[0].geometry.location;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 form.addEventListener('submit', (e: Event) => searchAddressHandler(e));
--- basic09/webpack.config.js
+++ basic09/webpack.config.js
@@ -2,36 +2,50 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 //es6 module의 .js 확장자를 다 지워야한다.
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
-  output: {
-    filename: 'bundle.js',
-    //에러를 방지하기 위해 tsconfig.json output path와 일치해야한다.
-    //절대경로로 입력
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        //tsconfig가 있음으로 추가구성 configuration을 명시하지 않아도 된다.
-        exclude: /node_modules/,
-      },
+module.exports = option => {
+  if (option.Dev) {
+    console.log('Dev environment start!');
+  }
+  dotenv.config();
+  if (!process.env.GOOGLE_API_KEY) throw Error('API_KEY is Empty!!');
+  return {
+    mode: 'development',
+    entry: './src/app.ts',
+    output: {
+      filename: 'bundle.js',
+      //에러를 방지하기 위해 tsconfig.json output path와 일치해야한다.
+      //절대경로로 입력
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+    },
+    devtool: false,
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          //tsconfig가 있음으로 추가구성 configuration을 명시하지 않아도 된다.
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    devServer: {
+      // historyApiFallback: true,
+      //브라우저를 통해 접근하는 경로 webpack.output.publicPath와 동일하게 설정
+      devMiddleware: { publicPath: '/dist/' },
+      //정적파일을 제공하는 경로
+      static: { directory: path.resolve(__dirname) },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        GOOGLE_API_KEY: JSON.stringify(process.env.GOOGLE_API_KEY) ?? '',
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  devServer: {
-    // historyApiFallback: true,
-    //브라우저를 통해 접근하는 경로 webpack.output.publicPath와 동일하게 설정
-    devMiddleware: { publicPath: '/dist/' },
-    //정적파일을 제공하는 경로
-    static: { directory: path.resolve(__dirname) },
-  },
+  };
 };
